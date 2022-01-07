6fa5155a16f7aaf0ebae1766c744c42ee802e58c
Jen <74936440+Jen04@users.noreply.github.com>
2022-01-07 17:35:33 -0500

    Updated text on webproject.php

--- pages/webproject.php
+++ pages/webproject.php
@@ -3,7 +3,8 @@
         <h1 class="projectHeader">Personal Website</h1>
         <img id="webIMG" src="images/websitecode.png" alt="Picture of website code" />
         <section id="webText">
-            <p>I wrote the code for this website using HTML and CSS.</p>
+            <p>I wrote the code for this website using HTML, CSS, and PHP. 
+                It was designed for desktop browsers.</p>
             <p>The code for this website can be found on GitHub here:</p>
             <a href="https://github.com/Jen04/Hynes-Tech"><button id="webBTN" type="button">Github Repository</button></a>
         </section>
