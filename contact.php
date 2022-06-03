757132b0e355a65762d93cd2d87912154f0383e3
Jen <74936440+Jen04@users.noreply.github.com>
2022-06-03 14:46:08 -0400

    Changed outdated URL

--- contact.php
+++ contact.php
@@ -1,14 +1,14 @@
 <?php include("header.html"); ?>
 
         <section id = "contact">
-            <img class = "Graduation" src="images/IMG-20210508-WA00032.jpg" alt="Picture of Jennifer Hynes" />
+            <img class = "Graduation" src="images/IMG-20210508-WA00032.jpg" alt="Picture of Jen Hynes" />
             <h1>Contact</h1>
             <p>The best way to reach me is through my email, jen@hynes.tech.</p>
         </section>
         <section id="links">
             <h1>Profile Links</h1>
             <div id="LinkedIn">
-                <a href="https://www.linkedin.com/in/jenniferhynes/">
+                <a href="https://www.linkedin.com/in/jenhynes/">
                     <img class = "LinkedInImg" src="images/In-Blue-Logo.png.original.png" alt="LinkedIn profile link" />
                     LinkedIn Profile</a> <br />
             </div>
--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <?php include("header.html"); ?>
 
         <section class = "header">
-            <img class = "Portrait" src="images/portrait.jpg" alt="Picture of Jennifer Hynes"/>
+            <img class = "Portrait" src="images/portrait.jpg" alt="Picture of Jen Hynes"/>
             <div id=headertext>
                 <p id="name">Hi, I'm <br /> Jen Hynes. </p>
                 <button type="button" class="aboutbtn" onclick="window.location.href='about.php';">About Me</button>
