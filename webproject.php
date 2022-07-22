93baba43f2bc097894bd1fa2b68d283b4aaf8f2a
Jen <74936440+Jen04@users.noreply.github.com>
2022-07-22 11:12:09 -0400

    Changed navbar ordering, improved webproject.php

--- webproject.php
+++ webproject.php
@@ -1,14 +1,14 @@
 <?php include("header.html"); ?>
+                <h1 class="projectHeader">Personal Website</h1>
                 <div class="row projectContents">
-                    <div class="col-lg-5 fadeInElement">
+                    <div class="fadeInElement">
                         <img class="img-fluid rounded mx-auto d-block projectImg" src="images/websitecode.png" alt="Picture of website code" />
                     </div>
-                    <div class="col-lg-7 fadeUp">
-                        <div class="projectText">
-                            <h2>Personal Website</h2>
+                    <div class="fadeUp">
+                        <div class="text-center projectText">
                             <p>This website is my current project.  As I learn more about web development, I will continue to improve it.</p>
-                            <p>The original version of this website, v1.0.0 on GitHub, was written using HTML, CSS, and PHP. It was designed with desktop browsers in mind. I wanted to improve my original design and make it mobile friendly, so I learned Bootstrap for v2.0.0.</p>
-                            <p>I am currently learning jQuery and plan to use what I learn to make more changes.</p>
+                            <p>The original version of this website, v1.0.0 on GitHub, was written using HTML, CSS, and PHP. It was designed with desktop browsers in mind.</p>
+                            <p>I wanted to improve my original design and make it mobile-friendly, so I learned Bootstrap for v2.0.0.</p>
                             <p>The code for this website can be found on GitHub here:</p>
                             <button type="button" id="webBtn" class="btn btn-success btn-lg" onclick="window.location.href='https://github.com/Jen04/Hynes-Tech';">GitHub Repository</button>
                             <p id=copyright>Copyright (c) 2022 Jennifer Hynes</p>
